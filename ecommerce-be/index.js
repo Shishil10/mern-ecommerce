b6f60eecb8d30f7603ae5c69f38ab59efc5cb4ba
Shishil10 <107184127+Shishil10@users.noreply.github.com>
2023-12-17 13:34:23 +0530

    Update index.js

--- ecommerce-be/index.js
+++ ecommerce-be/index.js
@@ -6,8 +6,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import nodemailer from "nodemailer";
 import cors from 'cors';
-import path from 'path';
-import {fileURLToPath } from 'url';
+
 
 //configure env
 dotenv.config();
@@ -15,25 +14,21 @@ dotenv.config();
 //database config
 connectDB();
 
-//esmodule fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 //rest object
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../ecommerce-fe/build')));
+
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-app.use('*', function (req, res) {
-   res.sendFile(path.join(__dirname,'../ecommerce-fe/build/index.html'));
-})
+
 
 
 
